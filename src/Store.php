112c8db519847d4c36e3d7f56eb7c22bfada4c17
Brendan Grubb <brendangrubb@gmail.com>
2017-03-03 10:03:51 -0800

    add functionality to save store to DB

--- src/Store.php
+++ src/Store.php
@@ -29,12 +29,24 @@
     // C R U D
         function save()
         {
-
+            $GLOBALS['DB']->exec("INSERT INTO stores (name) VALUES ('{$this->name}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
         {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores");
+            $stores = array();
 
+
+            foreach ($returned_stores as $store) {
+                $name = $store['name'];
+                $id = $store['id'];
+                $new_store = new Store($name, $id);
+                array_push($stores, $new_store);
+            }
+
+            return $stores;
         }
 
 
