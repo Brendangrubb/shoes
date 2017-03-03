cb05c33b353dd602121d7e12095254b1c3d74c33
Brendan Grubb <brendangrubb@gmail.com>
2017-03-03 09:32:55 -0800

    add test to get name of store

--- src/Store.php
+++ src/Store.php
@@ -3,5 +3,24 @@
     {
         private $name;
         private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+        }
+
+
+
+
+
+
+
+
+        
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,4 +1,5 @@
 <?php
+
     /**
     * @backupGlobals disabled
     * #backupStaticAttributes disabled
@@ -13,5 +14,19 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
+        function test_getName()
+        {
+            // Arrange
+            $name = "Shoe Mill";
+            $new_store = new Store($name);
+
+            // Act
+            $result = $new_store->getName();
+
+            // Assert
+            $this->assertEquals($name, $result);
+
+        }
     }
+
 ?>
