7fbfdb7622f90bf0fc20fbf22df95da9c6d88478
Brendan Grubb <brendangrubb@gmail.com>
2017-03-03 10:10:44 -0800

    add and test functionality to get all stores from DB

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -62,21 +62,37 @@
         }
 
     // TEST C R U D
-        function test_save()
+        // function test_save()
+        // {
+        //     // Arrange
+        //     $name = "Shoe Mill";
+        //     $new_store = new Store($name);
+        //     $new_store->save();
+        //
+        //     // Act
+        //     $result = Store::getAll();
+        //
+        //     // Assert
+        //     $this->assertEquals($new_store, $result[0]);
+        // }
+
+        function test_getAll()
         {
             // Arrange
             $name = "Shoe Mill";
             $new_store = new Store($name);
             $new_store->save();
 
+            $name2 = "Foot Traffic";
+            $new_store2 = new Store($name2);
+            $new_store2->save();
+
             // Act
             $result = Store::getAll();
 
             // Assert
-            $this->assertEquals($new_store, $result[0]);
+            $this->assertEquals([$new_store, $new_store2], $result);
         }
-
-
 
 
 
