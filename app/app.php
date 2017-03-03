cc2fe7435a65541fc56ad2ae2e67d77aaf831072
Brendan Grubb <brendangrubb@gmail.com>
2017-03-03 15:01:05 -0800

    add forms for entering stores and brands in database and list stores and brands on main page

--- app/app.php
+++ app/app.php
@@ -4,6 +4,14 @@
     require_once __DIR__.'/../src/Store.php';
     require_once __DIR__.'/../src/Brand.php';
 
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     use Symfony\Component\Debug\Debug;
     Debug::enable();
 
@@ -12,14 +20,27 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get('/', function() use ($app) {
+    $app->get('/', function() use ($app) {  // HOME PAGE WITH ENTER FORMS
+        $stores = Store::getAll();
+        $brands = Brand::getAll();
 
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('stores' => $stores, 'brands' => $brands));
     });
-    $app->get('/test', function() use ($app) {
 
-        return $app['twig']->render('test.html.twig');
+    $app->post('/add_store', function() use ($app) {  // ADD A STORE TO DB REDIRECTS HOME
+        $new_store = new Store($_POST['name']);
+        $new_store->save();
+
+        return $app->redirect('/');
     });
+
+    $app->post('/add_brand', function() use ($app) {  // ADD A BRAND TO DB REDIRECTS HOME
+        $new_brand = new Brand($_POST['name']);
+        $new_brand->save();
+
+        return $app->redirect('/');
+    });
+
 
     return $app;
 ?>
