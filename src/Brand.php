8cd6b3a9efe6bd5571b1a6ea256320c079827b52
Brendan Grubb <brendangrubb@gmail.com>
2017-03-03 11:03:33 -0800

    add functionality to set name of brand

--- src/Brand.php
+++ src/Brand.php
@@ -16,9 +16,9 @@
             return $this->name;
         }
 
-        function setName()
+        function setName($new_name)
         {
-            
+            $this->name = $new_name;
         }
 
     }
